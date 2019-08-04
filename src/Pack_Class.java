11b47036fd4d89e655fd1a54664f8d39c7e65eb8
Varun Kumar <varunprofessional91@gmail.com>
2019-08-04 23:20:55 +0530

    commit

--- src/Pack_Class.java
+++ src/Pack_Class.java
@@ -6,8 +6,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-//import com.amazonaws.services.lambda.runtime.Context;
-//import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.text.DateFormat;
@@ -22,13 +20,15 @@ public class Pack_Class {
         Date date = Calendar.getInstance().getTime();
         DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd hhmmss");
         DateFormat dateFormat2 = new SimpleDateFormat("yyyyMMdd");
+
         String strDate = dateFormat.format(date);
         String strDate2 = dateFormat2.format(date);
+
         String accessKey = "put access key here";    // example accessKey
         String secretKey = "put secret key here";    // example secretKey
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         AmazonS3 s3 = new AmazonS3Client(credentials, new ClientConfiguration().withProtocol(Protocol.HTTP));
-        String bucketName = "put bucket name here";    // example bucketName
+        String bucketName = "put bucket name here ";    // example bucketName
         String key = strDate2 + "/" + strDate + ".txt";
         InputStream input = new ByteArrayInputStream(new byte[0]);
         ObjectMetadata metadata = new ObjectMetadata();
